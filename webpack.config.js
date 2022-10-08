9b8daa81d532f3a98a382bf9ad945a0a448b38ec
dmitry-golofast <dmitry.golofast@gmail.com>
2022-10-08 22:56:32 +0300

    last fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,21 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                ],
-            },
-            {
                 test: /\.css$/,
                 use: [
-                    MiniCssExtractPlugin.loader, 'css-loader',
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader',
                 ],
             },
         ],
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
+        new HTMLWebpackPlugin(),
+        new MiniCSSExtractPlugin(),
     ],
 };
